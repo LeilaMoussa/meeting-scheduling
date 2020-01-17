791625f92a23da95de0bc005a3d02813e5a363dd
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2020-01-17 17:12:14 +0100

    Update meeting-time.py

--- meeting-time.py
+++ meeting-time.py
@@ -3,31 +3,27 @@
 def takeFirst(slot):
     return slot[0]
 
-## busy times on Friday
+## people's busy times on Friday
 
 salima = [["14:40", "15:30"]]
 ali = [["12:00", "12:50"], ["17:40", "19:20"]]
 fz = []
 reda = [["10:00", "10:50"], ["11:00", "11:50"]]
 salma = [["10:00", "10:50"], ["14:40", "15:30"]]
-youssef = [["09:00", "09:50"], ["10:00", "10:50"]]
-othmane = [["11:00", "11:50"]]
+aroune = []
+alae = []
 imane = [["10:00", "10:50"], ["14:40", "15:30"]]
+me = [["10:00", "10:50"], ["11:00", "11:50"], ["15:00", "16:50"]]
 
 ## find all common empty time slots
 
 """
 1. merge all lists into one list containing all busy times, sorted by starting time ascendingly
- 1.1. merge any two consecutive slots where (start1=start2 AND end1 = end2)
-                                         OR (end2 < end1)
-                                         OR (start1 = start2 AND end2 > end1)
-                                         OR (end1 = start2)
-                                         OR (start1 < start2 AND end1 = end2)
-                                         OR (start2 > start1 AND end2 < end1)
+ 1.1. merge any two consecutive slots if they overlap somehow
 2. return gaps
 """
 
-allBusy = salima + ali + fz + reda + salma + youssef + othmane + imane
+allBusy = salima + ali + fz + reda + salma + alae + aroune + imane + me
 allBusy.sort(key = takeFirst)
 
 i = 0
@@ -36,7 +32,7 @@ while (i < len(allBusy) - 1):  ## note that len changes constantly
     _next = allBusy[i+1]
     start1, end1 = this[0], this[1]
     start2, end2 = _next[0], _next[1]
-    if start1<start2 and end1<end2 and end1 != start2:
+    if start1<start2 and end1<end2 and end1 < start2:
         ## strictly separate slots
         i += 1
     else:
